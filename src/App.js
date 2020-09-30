cd9b94dd111ca5bfe5c16eb58a64e04d9933daac
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-09-30 12:02:58 +0100

    feat: RIL-26 add clickable map of london

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Location from "./components/Location/Location"
+import Location from "./components/Location/Location.jsx"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Location/>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     {/* </Location>  */}
