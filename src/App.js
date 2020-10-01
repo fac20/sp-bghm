705a405384988992612c4d4321e0e148fcde0c19
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-10-01 11:33:00 +0100

    fix: RIL-10 component path name changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound/PageNotFound.jsx";
 import Location from "./components/Location/Location.jsx";
-import RecyclingCategories from "./components/RecyclingCategories.jsx";
+import RecyclingCategories from "./components/RecyclingCategories/RecyclingCategories.jsx";
 
 function App() {
   return (
