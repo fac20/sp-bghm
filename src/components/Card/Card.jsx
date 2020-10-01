bd64f0e9507859db35afb9382da3dcb05d07b88c
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-10-01 15:04:26 +0100

    feat: RIL-1 added images and styling to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import blueBin from "../assets/wheelie-bin-blue.svg";
 
 const Container = styled.article`
   background-color: transparent;
@@ -8,6 +9,7 @@ const Container = styled.article`
   transform-style: preserve-3d;
   transition: transform 0.3s;
   width: 100%;
+  box-shadow: 0 0 1rem;
   &:focus {
     transform: rotateY(180deg);
   }
@@ -15,12 +17,12 @@ const Container = styled.article`
 
 const FrontBack = styled.div`
   backface-visibility: hidden;
-  background-color: hsl(159, 56%, 7%);
-  color: hsl(141, 45%, 81%);
+  background-color: var(--card-background-green);
+  color: var(--card-background-green);
   display: flex;
   flex-direction: column;
   height: 100%;
-  justify-content: space-evenly;
+  justify-content: space-between;
   position: absolute;
   text-align: center;
   width: 100%;
@@ -32,26 +34,42 @@ const Back = styled(FrontBack)`
   transform: rotateY(180deg);
 `;
 
-const Title = styled.h3``;
+const Title = styled.span`
+  background-color: var(--main-orange);
+  color: var(--background-dark-blue);
+`;
 
-const CategoryIcon = styled.img``;
+const CategoryIcon = styled.img`
+  height: 100%;
+  margin: 1rem;
+  width: auto;
+`;
 
 const CategoryBinImage = styled.img``;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: hsla(33, 98%, 50%, 0.8);
+  padding: 0.8rem;
+  font: inherit;
+  line-height: 1;
+  border: 1px;
+  margin: 0;
+`;
 
 function Card({ material, wasteOrganisation, src }) {
   return (
     <Container tabIndex="0">
       <Front>
-        <Title>{material}</Title>
+        <Title>
+          <h2>{material}</h2>
+        </Title>
         <CategoryIcon src={src} alt={material} />
       </Front>
       <Back>
         <Title>
-          {material} goes in {wasteOrganisation}
+          Put {material} in {wasteOrganisation}
         </Title>
-        <CategoryBinImage src="" alt="bin-type" />
+        <CategoryBinImage src={blueBin} alt="bin-type" />
         <Button>press for more information</Button>
       </Back>
     </Container>
--- src/components/RecyclingCategories/RecyclingCategories.jsx
+++ src/components/RecyclingCategories/RecyclingCategories.jsx
@@ -2,35 +2,68 @@ import React from "react";
 import Card from "../Card/Card.jsx";
 import styled from "styled-components";
 import plastic from "../assets/plastic.svg";
+import batteries from "../assets/batteries.svg";
+import gardenWaste from "../assets/garden-waste.svg";
+import glass from "../assets/glass.svg";
+import metal from "../assets/metal.svg";
+import otherWaste from "../assets/other-waste.svg";
+import paperAndCard from "../assets/paper-and-card.svg";
+import smallElectronics from "../assets/small-electronics.svg";
 
 const Div = styled.div`
-  background-color: hsl(129, 53%, 90%);
-  color: hsl(hsl(159, 56%, 7%));
+  background-color: var(--background-dark-blue);
+  color: var(--main-green);
+  text-align: center;
 `;
 const Section = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, 15rem);
   grid-gap: 1rem 1rem;
   width: 100%;
+  padding: 2rem;
+`;
+
+const Span = styled.span`
+  text-align: center;
+  margin: 2rem;
+`;
+
+const Button = styled.button`
+  background-color: hsla(33, 98%, 50%, 0.8);
+  padding: 0.8rem;
+  font: inherit;
+  line-height: 1;
+  border: 1px;
+  margin: 0;
 `;
 
 function RecyclingCategories() {
   return (
     <Div>
-      <h1>What type of recycling do you have?</h1>
+      <Span>
+        <h1>What type of recycling do you have?</h1>
+      </Span>
       <Section>
         <Card material="plastic" wasteOrganisation="" src={plastic} />
-        <Card material="paper and card" wasteOrganisation="" />
-        <Card material="metal" wasteOrganisation="" />
-        <Card material="glass" wasteOrganisation="" />
-        <Card material="garden waste" wasteOrganisation="" />
-        <Card material="small electronics" wasteOrganisation="" />
-        <Card material="batteries" wasteOrganisation="" />
-        <Card material="other waste" wasteOrganisation="" />
+        <Card
+          material="paper and card"
+          wasteOrganisation=""
+          src={paperAndCard}
+        />
+        <Card material="metal" wasteOrganisation="" src={metal} />
+        <Card material="glass" wasteOrganisation="" src={glass} />
+        <Card material="garden waste" wasteOrganisation="" src={gardenWaste} />
+        <Card
+          material="small electronics"
+          wasteOrganisation=""
+          src={smallElectronics}
+        />
+        <Card material="batteries" wasteOrganisation="" src={batteries} />
+        <Card material="other waste" wasteOrganisation="" src={otherWaste} />
       </Section>
-      <button>
+      <Button>
         Contact your local authority for other waste services and requests
-      </button>
+      </Button>
     </Div>
   );
 }
