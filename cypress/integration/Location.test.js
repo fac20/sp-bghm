a27c5549a6cb18e4c41fc8a59a2116e9b7ca1950
Rihards <rihards.jukna@gmail.com>
2020-10-06 14:47:54 +0100

    ril-50 test: add test for map and location change
    Co-authored-by: khadija-nur <59174800+khadija-nur@users.noreply.github.com>

--- cypress/integration/Location.test.js
+++ cypress/integration/Location.test.js
@@ -1,4 +1,4 @@
-describe("Location page tests: ", () => {
+describe("Select location from input form: ", () => {
   it("Location page succesfully loaded", () => {
     cy.visit("/");
   });
@@ -13,8 +13,34 @@ describe("Location page tests: ", () => {
   });
 
   it("Correct postcode input test:", () => {
-    cy.get("input[name=postcode]").type("NW101LG");
+    cy.get("input[name=postcode]").clear().type("NW101LG");
     cy.get("input[type=submit]").click();
     cy.get("h1").should("contain", "What type of recycling do you have?");
+    //Question: WHat's the difference between includes and contains
+    cy.url().should("include", "/categories/brent");
+  });
+
+  it("Change location from categories page: ", () => {
+    cy.go("back");
+  });
+});
+
+describe("Select location using map:", () => {
+  beforeEach(() => {
+    cy.get("svg>g>path").eq(20).click();
+  });
+  it("Background colour of clicked borough turns orange", () => {
+    cy.get("svg>g>path").eq(20).should("have.css", "fill", "rgb(230, 156, 65)");
+  });
+  it("Button appears after borough has been clicked", () => {
+    // click on lambeth
+
+    cy.get("main")
+      .find("button")
+      .get("h2")
+      .should("contain", "Lambeth")
+      .click();
+    cy.get("h1").should("contain", "What type of recycling do you have?");
+    cy.url().should("include", "/categories/lambeth");
   });
 });
