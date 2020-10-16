9050cb6157c1375eba724465a5119f573aff65c5
Trish Wynne <trishasone@gmail.com>
2020-10-16 08:23:53 +0100

    add tests and rebuild without process warnings as errors

--- src/pages/MoreInfo/MoreInfo.test.jsx
+++ src/pages/MoreInfo/MoreInfo.test.jsx
@@ -1,18 +1,18 @@
-// import React from "react";
-// import MoreInfo from "./MoreInfo.jsx";
-// import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
+import MoreInfo from "./MoreInfo.jsx";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-// describe("<MoreInfo />", () => {
-//   // Mock params from url
-//   const useParams = () => {
-//     return { borough: "haringey", material: "plastic" };
-//   };
+describe("<MoreInfo />", () => {
+  // Mock params from url
+  const useParams = () => {
+    return { borough: "haringey", material: "plastic" };
+  };
 
-//   test("The More Info page displays all sections", () => {
-//     render(<MoreInfo />);
-//     screen.getByText("What's recyclable?");
-//     screen.getByText("Where can I recycle");
-//     screen.getByText("What happens to my recycled");
-//     screen.getByText("The impact of recycling");
-//   });
-// });
+  test("The More Info page displays all sections", () => {
+    render(<MoreInfo />);
+    screen.getByText("What's recyclable?");
+    screen.getByText("Where can I recycle");
+    screen.getByText("What happens to my recycled");
+    screen.getByText("The impact of recycling");
+  });
+});
